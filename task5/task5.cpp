f0857a0fe8c705b60c1ef820ef437db8db79c954
Dmitrii Derendiaev <dimonsterus007@ya.ru>
2021-09-25 21:00:32 +0300

    task2 with errors AC_BC

--- task5/task5.cpp
+++ task5/task5.cpp
@@ -17,22 +17,37 @@ string task1(float x1, float y1, float x2, float y2)
 string task2(float A, float B, float C)
 {
 	string strok2;
-	float AC, BC, AC;
+	float AC, BC, AC_BC;
+	AC = abs(C - A);
+	BC = abs(C - B);
+	AC_BC = AC + BC;
+	strok2 += "AC = ";
+	strok2 += to_string(AC);
+	strok2 += "; BC = ";
+	strok2 += to_string(BC);
+	strok2 += "; SUM AC+BC = ";
+	strok2 += to_string(AC_BC);
+	return strok2;
 
 }
 
 int main()
 {
 	int z = 0;
-	switch (z) :
-		case 1:
+	float x1, x2, y1, y2, AC, BC, AC_BC, A, B, C;
+	switch (z)
+	{
+	case 1:
 	{
 		cin >> x1 >> y1 >> x2 >> y2;
 		cout << task1(x1, y1, x2, y2) << endl;
 	}
-		case 2:
-		{
+	case 2:
+	{
+		cin >> A >> B >> C;
+		cout << task2(A, B, C) << endl;
+	}
+	}
 
-			cin
-		}
+	return 0;
 }
