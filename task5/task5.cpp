fc8283c8e825ac67590d67eca711ba143819f6b0
Dmitrii Derendiaev <dimonsterus007@ya.ru>
2021-09-25 11:32:17 +0300

    Загрузка кода в task5

--- task5/task5.cpp
+++ task5/task5.cpp
@@ -1,20 +1,38 @@
-// task5.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+string task1(float x1, float y1, float x2, float y2)
+{
+	string strok1;
+	float L;
+	L = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+	strok1 += "Length = ";
+	strok1 += to_string(L);
+	return strok1;
+}
+
+string task2(float A, float B, float C)
+{
+	string strok2;
+	float AC, BC, AC;
+
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int z = 0;
+	switch (z) :
+		case 1:
+	{
+		cin >> x1 >> y1 >> x2 >> y2;
+		cout << task1(x1, y1, x2, y2) << endl;
+	}
+		case 2:
+		{
+
+			cin
+		}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
