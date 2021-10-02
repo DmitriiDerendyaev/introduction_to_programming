950ccefcc5d1930788bb362664c85a5278a4f2a6
Dmitrii Derendiaev <dimonsterus007@ya.ru>
2021-10-02 21:58:24 +0300

    task2_9 added a switch operator

--- task9/task9.cpp
+++ task9/task9.cpp
@@ -1,8 +1,86 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+int sec, day;
+
+string task1(int n)
+{
+	string strok1;
+	sec = n % 216000;
+	strok1 += "In last minute:";
+	strok1 += to_string(sec);
+
+	return strok1;
+}
+
+string task2(int day)
+{
+	string strok2;
+	day = day % 7;
+	switch (day)
+	{
+	case 0:
+	{
+		strok2 += "Current weekday: Sunday";
+		break;
+	}
+	case 1:
+	{
+		strok2 += "Current weekday: Monday";
+		break;
+	}
+	case 2:
+	{
+		strok2 += "Current weekday: Tuesday";
+		break;
+	}
+	case 3:
+	{
+		strok2 += "Current weekday: Wednesday";
+		break;
+	}
+	case 4:
+	{
+		strok2 += "Current weekday: Thursday";
+		break;
+	}
+	case 5:
+	{
+		strok2 += "Current weekday: Friday";
+		break;
+	}
+	case 6:
+	{
+		strok2 += "Current weekday: Saturday";
+		break;
+	}
+	}
+
+	return strok2;
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    int z;
+    cin >> z;
+
+	switch (z)
+	{
+	case 1:
+	{
+		cin >> sec;
+		cout << task1(sec);
+		break;
+	}
+	case 2:
+	{
+		cin >> day;
+		cout << task2(day);
+		break;
+	}
+	default:
+		break;
+	}
 }
