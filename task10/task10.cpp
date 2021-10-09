bc049ef9ebb5b80501ff06a8074403d7ec9e758a
Dmitrii Derendiaev <dimonsterus007@ya.ru>
2021-10-10 01:59:29 +0300

    created sample

---
+++ task10/task10.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string task1()
+{
+	return ;
+}
+
+int main()
+{
+	int z;
+	cin >> z;
+	switch (z)
+	{
+	case 1:
+	{
+
+	}
+	case 2:
+	{
+		
+	}
+	case 3:
+	{
+
+	}
+	case 4:
+	{
+
+	}
+	case 5:
+	{
+
+	}
+	default:
+		break;
+	}
+
+
+
+	return 0;
+}
